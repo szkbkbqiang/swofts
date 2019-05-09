ec92906888c24a07940a239da317ec75f3b72f98
miclefeng <miclefengzss@gmail.com>
2019-05-09 09:22:53 +0800

    edit

--- config/properties/app.php
+++ config/properties/app.php
@@ -8,23 +8,31 @@
  */
 
 return [
-    'env'          => env('APP_ENV', 'test'),
-    'debug'        => env('APP_DEBUG', false),
-    'version'      => '1.0',
+    'env' => env('APP_ENV', 'test'),
+    'debug' => env('APP_DEBUG', false),
+    'version' => '1.0',
     'autoInitBean' => true,
-    'bootScan'     => [
+    'bootScan' => [
         'App\Commands',
         'App\Boot',
     ],
-    'excludeScan'  => [
+    'excludeScan' => [
         'App\Helper',
     ],
-    'translator'         => [
+    'translator' => [
         'languageDir' => '@resources/languages/',
     ],
-    'db'           => require __DIR__ . DS . 'db.php',
-    'cache'        => require __DIR__ . DS . 'cache.php',
-    'service'      => require __DIR__ . DS . 'service.php',
-    'breaker'      => require __DIR__ . DS . 'breaker.php',
-    'provider'     => require __DIR__ . DS . 'provider.php',
+    'db' => require __DIR__ . DS . 'db.php',
+    'cache' => require __DIR__ . DS . 'cache.php',
+    'service' => require __DIR__ . DS . 'service.php',
+    'breaker' => require __DIR__ . DS . 'breaker.php',
+    'provider' => require __DIR__ . DS . 'provider.php',
+    'devtool' => [
+        // 是否开启 DevTool，默认值为 false
+        'enable' => true,
+        // (可选)前台运行服务器时，是否打印事件调用到 Console
+        'logEventToConsole' => true,
+        // (可选)前台运行服务器时，是否打印 HTTP 请求到 Console
+        'logHttpRequestToConsole' => true,
+    ],
 ];
