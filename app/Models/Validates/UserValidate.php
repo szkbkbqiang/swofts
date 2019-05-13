3b439873975cc4c95537bdb0f3e6d04cb6fbb4fd
miclefeng <miclefengzss@gmail.com>
2019-05-13 11:07:27 +0800

    edit

---
+++ app/Models/Validates/UserValidate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: zhang.shangshang@tslsmart.com
+ * Date: 2019/5/13 上午10:58
+ */
+
+namespace App\Models\Validates;
+
+
+use Swoft\Bean\Annotation\Bean;
+use App\Utils\Validate\Validate;
+
+/**
+ * @Bean()
+ * Class UserValidate
+ * @package App\Models\Validates
+ */
+class UserValidate
+{
+
+}
